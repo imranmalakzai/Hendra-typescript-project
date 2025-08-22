73027936da1682aa8a353fd8d5cfe7cbc2ccfc44
imranmalakzai <afghanimran92@gmail.com>
2025-08-22 22:11:06 +0430

    create 02.any.ts
    This file contain any key type
    - if we give a varible type any it can contain any ...
    - if we don't specify the type it migh compaline after redefining the varible to another type

---
+++ project/02-any.ts
@@ -0,0 +1,10 @@
+
+
+// once somting assing to someting that conat be change  to another type
+
+let name : any = "khan";
+name = true;
+
+//IF we don't spsify the any key workd type script my compalin
+let fater_name = "Haji";
+fater_name = true;
