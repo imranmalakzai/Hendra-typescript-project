cad23f4392c10396cf227eb092abc6454c36e6e0
imranmalakzai <afghanimran92@gmail.com>
2025-08-23 19:39:46 +0430

    created 18-cast.ts
    In type script there are thow way to cast a variable
    1. Using as keyword
    2. Using angle bracket syntax

---
+++ project/18-cast.ts
@@ -0,0 +1,10 @@
+// In type script there are two ways for casting a variable
+// 1. Using angle bracket syntax
+// 2. Using as syntax
+
+// Here is an example of both wa
+
+let name: unknown = "Imran Malakzai";
+
+name = (name as number) = 19; // Using 'as' syntax
+name = (<number>name) = 123432432432;
