93be27bf26b42a4fcdaf57a9cb868690bf429eb7
imranmalakzai <afghanimran92@gmail.com>
2025-08-23 09:23:18 +0430

    create 12-enum.ts
    Inum help to give pedefine option for selecting
    -enum has a defult value 0 and one 1,
    - enum gender : {female = female,male=male};

---
+++ project/12-enum.ts
@@ -0,0 +1,15 @@
+// Enums help to choce the predefin options
+
+enum gender {
+  male = "male",
+  famele = "female"
+}
+
+let you :{name:string,gender:gender,age:number,selary?:number};
+
+you = {
+  name:"Imran Malakzai",
+  selary:2000,
+  gender:gender.famele,
+  age:21
+}
