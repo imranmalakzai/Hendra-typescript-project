b64220cb619e9c6064f462c9a52413f0c3f5cc8d
imranmalakzai <afghanimran92@gmail.com>
2025-08-23 09:42:44 +0430

    create 14-Interface.ts
    Interfaces are similar to type aliases, except they only apply to object types.
    - when we defin type Interface it can use in several places
    - ex: interface Car {name:string,model:number}

---
+++ project/14-Interface.ts
@@ -0,0 +1,16 @@
+//Interfaces are similar to type aliases, except they only apply to object types.
+
+
+interface News {
+  N :string,
+  E : string,
+  w : string,
+  s : string
+}
+
+const meaningOfNews:News = {
+  N: "Nort",
+  w: "west",
+  s: "south",
+  E: "East"
+}
