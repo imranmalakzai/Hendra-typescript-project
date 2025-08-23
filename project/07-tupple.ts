83a5aef18551ea6c4f7230f15af93cb35a88f34a
imranmalakzai <afghanimran92@gmail.com>
2025-08-23 08:35:39 +0430

    create 07-tupple.ts
    Tupple when we predefine an Array inside Element it clalled tupple
    - Tuple Element can't be changes
    - ex: const names:[string,string] = ["imran","imran"] lenght and Element can't be changed

---
+++ project/07-tupple.ts
@@ -0,0 +1,12 @@
+
+// Tuble define the correct order for our type
+let ourTuple:[string,number,boolean,null,undefined,false,22];
+ourTuple=["Imran",133,true,null,undefined,false,22];
+
+// By the correct order that doesnot complain
+
+// If we change the order it will complain
+
+let names:[string,string,string];
+
+names = ["Imran",1,"imran"]
